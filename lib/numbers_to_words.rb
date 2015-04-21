efdb326e260309277213ac618af38c3e7c3b66db
Guest User <student@epicodus.com>
2015-04-21 14:52:26 -0700

    working code skeleton for thousands+ places

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -1,25 +1,49 @@
 class String
   def numbers_to_words
+
+
+
+
     word_result = ""
 
     single_digit = {
-      "1" => "one"
+      "1" => "one",
+      "2"=> "two"
     }
 
     double_digit = {
       "2" => "twenty"
     }
 
-
       if self.length == 1
         word_result = single_digit.fetch(self)
-      else self.length == 2
+      elsif self.length == 2
         two_items = self.split("")
-        word_result = double_digit.fetch(two_items[0]) + "-" + single_digit.fetch(two_items[1])
+        word_result = double_digit.fetch(two_items[0]) + "-" +              single_digit.fetch(two_items[1])
+      elsif self.length == 3
+        three_items = self.split("")
+        word_result = single_digit.fetch(three_items[0]) + "-hundred-" + double_digit.fetch(three_items[1]) + "-" +              single_digit.fetch(three_items[2])
       end
-
 
     word_result
 
   end
+
+  def format_number
+    formatted_number = self.reverse.gsub(/(\d{3})(?=\d)/, '\\1,').reverse.split(",")
+
+#    formatted_number = self.split(",")
+
+    if formatted_number[-2]
+      formatted_number[-2].numbers_to_words + "thousand" +     formatted_number[-1].numbers_to_words
+
+    else
+      formatted_number[-1]
+      formatted_number[-1].numbers_to_words
+    end
+
+  end
+
+
+
 end
--- spec/numbers_to_words_spec.rb
+++ spec/numbers_to_words_spec.rb
@@ -13,7 +13,10 @@ describe('numbers to words') do
   end
 
   it('converts a three-digit number to its word equivilent') do
-    expect("121".numbers_to_words()).to(eq("one-hundred- twenty-one"))
+    expect("121".numbers_to_words()).to(eq("one-hundred-twenty-one"))
   end
 
+  it('adds commas in the corredt places into a number') do
+    expect("1222".format_number()).to(eq("1,222"))
+  end
 end
